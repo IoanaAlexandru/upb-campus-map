f2f93e7acb8f1221e1702432830e5a173f84d91e
Mark Grigore <edwmrkg@gmail.com>
2019-05-14 18:44:52 +0300

    Undo zoom in & out and add tabs to ED

--- app/src/main/java/com/example/upbcampus/EcActivity.kt
+++ app/src/main/java/com/example/upbcampus/EcActivity.kt
@@ -4,99 +4,45 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import android.support.v4.view.ViewCompat.animate
-import android.R.attr.x
-import android.R.attr.y
-import android.R.attr.start
-import android.annotation.SuppressLint
-import android.view.MotionEvent
-import android.view.View
-import android.view.View.OnTouchListener
-import android.support.v4.view.ViewCompat.setY
-import android.support.v4.view.ViewCompat.setX
-import android.R.attr.y
-import android.R.attr.x
-
-
-
-
-
+import uk.co.senab.photoview.PhotoViewAttacher
 
 
 class EcActivity : AppCompatActivity() {
 
-
-    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ec)
 
         val buttonFloor1 = findViewById(R.id.ec_floor1) as? Button
         val buttonFloor2 = findViewById(R.id.ec_floor2) as? Button
-        val zoomIn = findViewById(R.id.zoom_in) as? Button
-        val zoomOut = findViewById(R.id.zoom_out) as? Button
         val image = findViewById(R.id.ecImage) as? ImageView
+        image?.setImageResource(R.drawable.ec_parter)
 
-        var dx = 0
-        var dy = 0
-
-        image?.setOnTouchListener(object : View.OnTouchListener {
-            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
-                when (event?.getAction()) {
-                    MotionEvent.ACTION_DOWN -> {
-                        val x = event.getX()
-                        val y = event.getY()
-                        dx = (x - image.getX()).toInt()
-                        dy = (y - image.getY()).toInt()
-                    }
-                    MotionEvent.ACTION_MOVE -> {
-                        image.setX(event.getX() - dx)
-                        image.setY(event.getY() - dy)
-                    }
-                }
-                return true
-            }
-        })
+        val photoView = PhotoViewAttacher(image)
+        photoView.update()
 
         buttonFloor1?.setOnClickListener {
-                println("You clicked on floor1")
-                //image?.setImageResource(R.drawable.ec_parter)
-                image?.setBackgroundResource(R.drawable.ec_parter)
-                buttonFloor1.setBackgroundColor(resources.getColor(R.color.colorGray))
-                buttonFloor1.setTextColor(resources.getColor(R.color.colorBlack))
-                buttonFloor2?.setBackgroundColor(resources.getColor(R.color.colorWhite))
-                buttonFloor2?.setTextColor(resources.getColor(R.color.colorDarkGray))
+            println("You clicked on floor1")
+            image?.setImageResource(R.drawable.ec_parter)
+            setButtonStyleBlack(buttonFloor1)
+            setButtonStyleGray(buttonFloor2)
         }
 
         buttonFloor2?.setOnClickListener {
-                println("You clicked on floor2")
-                image?.setBackgroundResource(R.drawable.ec_etaj)
-                buttonFloor2.setBackgroundColor(resources.getColor(R.color.colorGray))
-                buttonFloor2.setTextColor(resources.getColor(R.color.colorBlack))
-                buttonFloor1?.setBackgroundColor(resources.getColor(R.color.colorWhite))
-                buttonFloor1?.setTextColor(resources.getColor(R.color.colorDarkGray))
+            println("You clicked on floor2")
+            image?.setImageResource(R.drawable.ec_etaj)
+            setButtonStyleBlack(buttonFloor2)
+            setButtonStyleGray(buttonFloor1)
         }
+    }
 
-        zoomIn?.setOnClickListener {
-            println("Zooming in...")
-            val x = image?.getScaleX()
-            val y = image?.getScaleY()
+    fun setButtonStyleGray(button: Button?) {
+        button?.setBackgroundColor(resources.getColor(R.color.colorWhite))
+        button?.setTextColor(resources.getColor(R.color.colorDarkGray))
+    }
 
-            if (x != null)
-                image?.setScaleX(x + 1)
-            if (y != null)
-                image?.setScaleY(y + 1)
-        }
-
-        zoomOut?.setOnClickListener {
-            println("Zooming out...")
-            val x = image?.getScaleX()
-            val y = image?.getScaleY()
-
-            if (x != null)
-                image?.setScaleX(x - 1)
-            if (y != null)
-                image?.setScaleY(y - 1)
-        }
+    fun setButtonStyleBlack(button: Button?) {
+        button?.setBackgroundColor(resources.getColor(R.color.colorGray))
+        button?.setTextColor(resources.getColor(R.color.colorBlack))
     }
 }
--- app/src/main/java/com/example/upbcampus/EdActivity.kt
+++ app/src/main/java/com/example/upbcampus/EdActivity.kt
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import uk.co.senab.photoview.PhotoViewAttacher
 
 class EdActivity : AppCompatActivity() {
 
@@ -16,25 +17,55 @@ class EdActivity : AppCompatActivity() {
         val buttonFloor3 = findViewById(R.id.ed_floor3) as? Button
         val buttonFloor4 = findViewById(R.id.ed_floor4) as? Button
         val image = findViewById(R.id.edImage) as? ImageView
+        image?.setImageResource(R.drawable.ed_parter)
+
+        val photoView = PhotoViewAttacher(image)
+        photoView.update()
 
         buttonFloor1?.setOnClickListener {
             println("You clicked on floor1")
             image?.setImageResource(R.drawable.ed_parter)
+            setButtonStyleBlack(buttonFloor1)
+            setButtonStyleGray(buttonFloor2)
+            setButtonStyleGray(buttonFloor3)
+            setButtonStyleGray(buttonFloor4)
         }
 
         buttonFloor2?.setOnClickListener {
             println("You clicked on floor2")
             image?.setImageResource(R.drawable.ed_etaj)
+            setButtonStyleBlack(buttonFloor2)
+            setButtonStyleGray(buttonFloor1)
+            setButtonStyleGray(buttonFloor3)
+            setButtonStyleGray(buttonFloor4)
         }
 
         buttonFloor3?.setOnClickListener {
             println("You clicked on floor2")
             image?.setImageResource(R.drawable.ed_etaj2)
+            setButtonStyleBlack(buttonFloor3)
+            setButtonStyleGray(buttonFloor1)
+            setButtonStyleGray(buttonFloor2)
+            setButtonStyleGray(buttonFloor4)
         }
 
         buttonFloor4?.setOnClickListener {
             println("You clicked on floor2")
             image?.setImageResource(R.drawable.ed)
+            setButtonStyleBlack(buttonFloor4)
+            setButtonStyleGray(buttonFloor2)
+            setButtonStyleGray(buttonFloor3)
+            setButtonStyleGray(buttonFloor1)
         }
+    }
+
+    fun setButtonStyleGray(button: Button?) {
+        button?.setBackgroundColor(resources.getColor(R.color.colorWhite))
+        button?.setTextColor(resources.getColor(R.color.colorDarkGray))
+    }
+
+    fun setButtonStyleBlack(button: Button?) {
+        button?.setBackgroundColor(resources.getColor(R.color.colorGray))
+        button?.setTextColor(resources.getColor(R.color.colorBlack))
     }
 }
