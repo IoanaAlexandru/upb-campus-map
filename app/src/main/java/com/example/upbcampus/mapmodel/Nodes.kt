0fbaa9925547b979601bdf88f9ae3cfb7abc9267
Ioana Alexandru <ioanaa.alexandru98@gmail.com>
2019-05-15 15:03:19 +0300

    add neighbors to nodes

--- app/src/main/java/com/example/upbcampus/mapmodel/Nodes.kt
+++ app/src/main/java/com/example/upbcampus/mapmodel/Nodes.kt
@@ -1,46 +1,85 @@
 package com.example.upbcampus.mapmodel
 
-enum class Building { EC, ED, UNKNOWN }
+enum class Building { EC, ED }
 
 abstract class Node(
-    val id: Int,
-    val name: String,
-    val floor: Int,
-    val building: Building
+    val id: Int, val name: String, val floor: Int, val building: Building
 )
 
-class Entrance(id: Int, name: String, floor: Int, building: Building) :
+class Entrance(
+    id: Int, name: String, floor: Int, building: Building,
+    val from: Int?, val to: Int
+) :
     Node(id, name, floor, building)
 
-class Stairs(id: Int, name: String, floor: Int, building: Building) :
+abstract class Vertical(
+    id: Int, name: String, floor: Int, building: Building,
+    val up: Int?, val down: Int?
+) :
     Node(id, name, floor, building)
 
-class Intersection(id: Int, name: String, floor: Int, building: Building) :
+class Stairs(
+    id: Int, name: String, floor: Int, building: Building,
+    up: Int?, down: Int?
+) :
+    Vertical(id, name, floor, building, up, down)
+
+class Elevator(
+    id: Int, name: String, floor: Int, building: Building,
+    up: Int, down: Int
+) :
+    Vertical(id, name, floor, building, up, down)
+
+class Intersection(
+    id: Int, name: String, floor: Int, building: Building,
+    val north: Int?, val south: Int?, val east: Int?, val west: Int?
+) :
     Node(id, name, floor, building)
 
-class Elevator(id: Int, name: String, floor: Int, building: Building) :
+abstract class Room(
+    id: Int, name: String, floor: Int, building: Building,
+    val neighbor: Int
+) :
     Node(id, name, floor, building)
 
-abstract class Room(id: Int, name: String, floor: Int, building: Building) :
-    Node(id, name, floor, building)
+class LectureHall(
+    id: Int, name: String, floor: Int, building: Building,
+    neighbor: Int
+) :
+    Room(id, name, floor, building, neighbor)
 
-class LectureHall(id: Int, name: String, floor: Int, building: Building) :
-    Room(id, name, floor, building)
+class Laboratory(
+    id: Int, name: String, floor: Int, building: Building,
+    neighbor: Int
+) :
+    Room(id, name, floor, building, neighbor)
 
-class Laboratory(id: Int, name: String, floor: Int, building: Building) :
-    Room(id, name, floor, building)
+class Restroom(
+    id: Int, name: String, floor: Int, building: Building,
+    neighbor: Int
+) :
+    Room(id, name, floor, building, neighbor)
 
-class Restroom(id: Int, name: String, floor: Int, building: Building) :
-    Room(id, name, floor, building)
+class Classroom(
+    id: Int, name: String, floor: Int, building: Building,
+    neighbor: Int
+) :
+    Room(id, name, floor, building, neighbor)
 
-class Classroom(id: Int, name: String, floor: Int, building: Building) :
-    Room(id, name, floor, building)
+class Office(
+    id: Int, name: String, floor: Int, building: Building,
+    neighbor: Int
+) :
+    Room(id, name, floor, building, neighbor)
 
-class Office(id: Int, name: String, floor: Int, building: Building) :
-    Room(id, name, floor, building)
+class Store(
+    id: Int, name: String, floor: Int, building: Building,
+    neighbor: Int
+) :
+    Room(id, name, floor, building, neighbor)
 
-class Store(id: Int, name: String, floor: Int, building: Building) :
-    Room(id, name, floor, building)
-
-class UnknownRoom(id: Int, name: String, floor: Int, building: Building) :
-    Room(id, name, floor, building)
+class UnknownRoom(
+    id: Int, name: String, floor: Int, building: Building,
+    neighbor: Int
+) :
+    Room(id, name, floor, building, neighbor)
