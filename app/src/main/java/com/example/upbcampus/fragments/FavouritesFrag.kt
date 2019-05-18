a20a9ecce8e50b10565ab47f14057e898b80eca6
Ioana Alexandru <ioanaa.alexandru98@gmail.com>
2019-05-18 17:46:18 +0300

    save favourites persistently

--- app/src/main/java/com/example/upbcampus/fragments/FavouritesFrag.kt
+++ app/src/main/java/com/example/upbcampus/fragments/FavouritesFrag.kt
@@ -8,21 +8,20 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import com.example.upbcampus.R
+import com.example.upbcampus.model.UPBUser
 
 class FavouritesFrag : Fragment() {
 
     private var listAdapter : ArrayAdapter<String>? = null
-    var favouritesList : MutableList<String> = ArrayList()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         val rootView = inflater.inflate(R.layout.fragment_favourite, container, false)
         val listView = rootView.findViewById<View>(R.id.lv_fav) as? ListView
 
-        listAdapter = ArrayAdapter(context, android.R.layout.simple_list_item_1, favouritesList)
+        listAdapter = ArrayAdapter(context, android.R.layout.simple_list_item_1, UPBUser.favouritesList)
         listView?.adapter = listAdapter
         listView?.transcriptMode = ListView.TRANSCRIPT_MODE_ALWAYS_SCROLL
         listView?.isStackFromBottom = true
@@ -32,7 +31,5 @@ class FavouritesFrag : Fragment() {
         return rootView
     }
 
-    fun getList() : MutableList<String> {
-        return  favouritesList
-    }
+    fun notify() = listAdapter?.notifyDataSetChanged()
 }
--- app/src/main/java/com/example/upbcampus/model/UPBUser.kt
+++ app/src/main/java/com/example/upbcampus/model/UPBUser.kt
@@ -10,6 +10,7 @@ import java.io.FileWriter
 import java.util.*
 
 const val HISTORY_FILE = "history.json"
+const val FAVOURITES_FILE = "favourites.json"
 
 private class Query(var query: String, var lastSearched: Date, var frequency: Int = 1) : Comparable<Query> {
     override fun compareTo(other: Query): Int {
@@ -19,25 +20,38 @@ private class Query(var query: String, var lastSearched: Date, var frequency: In
 
 // Singleton with lazy initialisation
 object UPBUser {
-    private val queries : MutableMap<String, Query>
+    private val queries: MutableMap<String, Query>
     private val queriesByLastSearched = TreeSet<Query>(compareBy { si -> si.lastSearched })
     private val queriesByFrequency = TreeSet<Query>(compareBy { si -> si.frequency })
 
     val historyList: MutableList<String>
+    val favouritesList: MutableList<String>
 
     init {
         val gson = Gson()
 
-        val historyFile = File(App.mFilesDir, HISTORY_FILE)
-        val reader = FileReader(historyFile)
+        // Fetch history data from file
+        val historyReader = getFileReader(HISTORY_FILE)
 
         val queryListType = object : TypeToken<List<Query>>() {}.type
-        val savedQueries = gson.fromJson<List<Query>>(reader, queryListType)
+        val savedQueries = gson.fromJson<List<Query>>(historyReader, queryListType) ?: mutableListOf()
 
-        queries = savedQueries.map { query -> query.query to query}.toMap().toMutableMap()
+        queries = savedQueries.map { query -> query.query to query }.toMap().toMutableMap()
         historyList = queries.keys.toMutableList()
+
+        // Fetch favourites from file
+        val favouritesReader = getFileReader(FAVOURITES_FILE)
+
+        val stringListType = object : TypeToken<MutableList<String>>() {}.type
+        favouritesList = gson.fromJson<MutableList<String>>(favouritesReader, stringListType) ?: mutableListOf()
     }
 
+    private fun getFileReader(fileName: String): FileReader {
+        val file = File(App.mFilesDir, fileName)
+        if (!file.exists())
+            file.createNewFile()
+        return FileReader(file)
+    }
 
     fun search(query: String, date: Date) {
         if (queries.contains(query)) {
@@ -52,19 +66,31 @@ object UPBUser {
         historyList.add(query)
     }
 
+    fun addToFavourites(name: String) = favouritesList.add(name)
+
+    fun removeFromFavourites(name: String) = favouritesList.remove(name)
+
+    fun isInFavourites(name: String) = favouritesList.contains(name)
+
     fun saveData() {
         val gson = Gson()
         val queriesJson = gson.toJson(queries.values)
+        val favouritesJson = gson.toJson(favouritesList)
 
+        writeDataToFile(queriesJson, HISTORY_FILE)
+        writeDataToFile(favouritesJson, FAVOURITES_FILE)
+    }
+
+    private fun writeDataToFile(data: String, file: String) {
         try {
-            val historyFile = File(App.mFilesDir, HISTORY_FILE)
+            val historyFile = File(App.mFilesDir, file)
             val writer = FileWriter(historyFile)
-            writer.append(queriesJson)
+            writer.append(data)
             writer.flush()
             writer.close()
         } catch (e: Exception) {
             e.printStackTrace()
-            Log.e(this::class.java.simpleName, "Couldn't save history to storage")
+            Log.e(this::class.java.simpleName, "Couldn't write data to $file")
         }
     }
 }
