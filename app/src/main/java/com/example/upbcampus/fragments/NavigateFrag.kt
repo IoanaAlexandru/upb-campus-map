4a756beea3377a2a7af484621e181440395cd676
Ioana Alexandru <ioanaa.alexandru98@gmail.com>
2019-05-19 13:38:21 +0300

    move navigation strings to strings.xml

--- app/src/main/java/com/example/upbcampus/fragments/NavigateFrag.kt
+++ app/src/main/java/com/example/upbcampus/fragments/NavigateFrag.kt
@@ -31,19 +31,9 @@ class NavigateFrag : Fragment() {
         data = ArrayList()
 
         /* Complete ListView with direction texts and its images */
-        directions.add(start_from) // do not delete this line
 
-        // TODO: This are just values for debugging, delete them after fetching data from UPBMap and uncomment the line below
+        // TODO fix navigation
         // directions = UPBMap.navigate(UPBUser.src, room)
-
-        directions.add(right_side)
-        directions.add(left_side)
-        directions.add(climb)
-        directions.add(descend)
-        directions.add(enter_building)
-        directions.add(front_side)
-
-        directions.add(reach_dest)  // do not delete this line
 
         for (i in 0 until directions.size) {
             image.add(getDirectionImage(directions[i]))
@@ -60,65 +50,66 @@ class NavigateFrag : Fragment() {
             )
         }
 
-        var customAdapter = NavigateAdapter(data, context!!)
+        val customAdapter = NavigateAdapter(data, context!!)
         listView!!.adapter = customAdapter
 
         return rootView
     }
 
+    // TODO Each Direction should have a getImage() method
     private fun getDirectionImage(direction: String): Int {
-        when (direction) {
-            descend -> {
-                return R.drawable.ic_stairs_mirrored            }
-            climb -> {
-                return R.drawable.ic_stairs
-            }
-            floors -> {
-                return R.drawable.baseline_unfold_more_24
-            }
-            to_floor -> {
-                return R.drawable.baseline_unfold_more_24
-            }
-            start_from -> {
-                return R.drawable.baseline_my_location_24
-            }
-            room -> {
-                return R.drawable.baseline_account_balance_24
-            }
-            is_on_side -> {
-                return R.drawable.baseline_account_balance_24
-            }
-            left_side -> {
-                return R.drawable.baseline_arrow_back_24
-            }
-            right_side -> {
-                return R.drawable.baseline_arrow_forward_24
-            }
-            front_side -> {
-                return R.drawable.baseline_arrow_upward_24
-            }
-            reach_dest -> {
-                return R.drawable.baseline_pin_drop_24
-            }
-            enter_building -> {
-                return R.drawable.ic_door
-            }
-            go_right -> {
-                return R.drawable.baseline_arrow_forward_24
-            }
-            go_left -> {
-                return R.drawable.baseline_arrow_back_24
-            }
-            go_forward -> {
-                return R.drawable.baseline_arrow_upward_24
-            }
-            go_back -> {
-                return R.drawable.baseline_arrow_downward_24
-            }
-            towards -> {
-                return R.drawable.baseline_arrow_downward_24
-            }
-        }
+//        when (direction) {
+//            descend -> {
+//                return R.drawable.ic_stairs_mirrored            }
+//            climb -> {
+//                return R.drawable.ic_stairs
+//            }
+//            floors -> {
+//                return R.drawable.baseline_unfold_more_24
+//            }
+//            to_floor -> {
+//                return R.drawable.baseline_unfold_more_24
+//            }
+//            start_from -> {
+//                return R.drawable.baseline_my_location_24
+//            }
+//            room -> {
+//                return R.drawable.baseline_account_balance_24
+//            }
+//            is_on_side -> {
+//                return R.drawable.baseline_account_balance_24
+//            }
+//            left_side -> {
+//                return R.drawable.baseline_arrow_back_24
+//            }
+//            right_side -> {
+//                return R.drawable.baseline_arrow_forward_24
+//            }
+//            front_side -> {
+//                return R.drawable.baseline_arrow_upward_24
+//            }
+//            reach_dest -> {
+//                return R.drawable.baseline_pin_drop_24
+//            }
+//            enter_building -> {
+//                return R.drawable.ic_door
+//            }
+//            go_right -> {
+//                return R.drawable.baseline_arrow_forward_24
+//            }
+//            go_left -> {
+//                return R.drawable.baseline_arrow_back_24
+//            }
+//            go_forward -> {
+//                return R.drawable.baseline_arrow_upward_24
+//            }
+//            go_back -> {
+//                return R.drawable.baseline_arrow_downward_24
+//            }
+//            towards -> {
+//                return R.drawable.baseline_arrow_downward_24
+//            }
+//        }
         return 0
     }
 }
--- app/src/main/java/com/example/upbcampus/model/Directions.kt
+++ app/src/main/java/com/example/upbcampus/model/Directions.kt
@@ -1,11 +1,14 @@
 package com.example.upbcampus.model
 
+import com.example.upbcampus.R
 import com.example.upbcampus.utils.*
 
 abstract class Direction(
     val src: Node,
     val dst: Node
 )
+
+private fun getStr(id: Int) = App.mResources?.getString(id)
 
 class LocationDirection(loc: Node, private val info: Heading) :
     Direction(loc, loc)
@@ -14,13 +17,13 @@ class LocationDirection(loc: Node, private val info: Heading) :
         var message = ""
 
         message += if (info == Heading.UNKNOWN)
-            "$start_from " + src.name + "."
+            "${getStr(R.string.start_from)} ${src.name}."
         else
             when(info) {
-                Heading.FORWARD -> "$room $front_side."
-                Heading.RIGHT -> "$room $is_on_side $right_side."
-                Heading.LEFT -> "$room $is_on_side $left_side."
-                else -> "$reach_dest."
+                Heading.FORWARD -> "${getStr(R.string.room)} ${getStr(R.string.front_side)}."
+                Heading.RIGHT -> "${getStr(R.string.room)} ${getStr(R.string.is_on_side)} ${getStr(R.string.right_side)}."
+                Heading.LEFT -> "${getStr(R.string.room)} ${getStr(R.string.is_on_side)} ${getStr(R.string.left_side)}."
+                else -> "${getStr(R.string.reach_dest)}."
             }
 
         return message
@@ -30,7 +33,7 @@ class LocationDirection(loc: Node, private val info: Heading) :
 class EntranceDirection(private val loc: Node)
     : Direction (loc, loc) {
     override fun toString(): String {
-        return enter_building + " " + loc.name + "."
+        return "${getStr(R.string.enter_building)} ${loc.name}."
     }
 }
 
@@ -45,10 +48,10 @@ class VerticalDirection(src: Node, dst: Node) :
         val x = getElevation()
         var message = ""
 
-        message += if (x > 0) climb else descend
+        message += if (x > 0) getStr(R.string.climb) else getStr(R.string.descend)
 
-        message += "" + Math.abs(x) + " " + floors
-        message += " ($to_floor " + dst.floor + ")"
+        message += "" + Math.abs(x) + " " + getStr(R.string.floors)
+        message += " (${getStr(R.string.to_floor)} ${dst.floor})"
 
         return message
     }
